790a2837f0c1421475e6364290c15c65b6f18517
vipyne <vipyne@gmail.com>
2025-04-24 16:07:41 -0500

    first take at adding coval ci

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def main(room_url: str, token: str):
     messages = [
         {
             "role": "system",
-            "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
+            "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way. You know a lot about laserdiscs.",
         },
     ]
 
