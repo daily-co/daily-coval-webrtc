ded7c5647507a56e1e48fa807c0bf37313f4fc16
vipyne <vipyne@gmail.com>
2025-06-04 12:30:58 -0500

    add custom observer to bot

--- bot.py
+++ bot.py
@@ -20,6 +20,22 @@ from pipecat.services.openai.llm import OpenAILLMService
 from pipecat.transports.services.daily import DailyParams, DailyTransport
 from pipecatcloud.agent import DailySessionArguments
 
+from pipecat.frames.frames import (
+    EndFrame,
+    ErrorFrame,
+    TTSSpeakFrame,
+    EndTaskFrame,
+    BotStartedSpeakingFrame,
+    UserStartedSpeakingFrame,
+    BotStoppedSpeakingFrame,
+    UserStoppedSpeakingFrame,
+    TTSTextFrame,
+    LLMFullResponseStartFrame,
+)
+from pipecat.observers.base_observer import BaseObserver, FramePushed
+from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
+from pipecat.pipeline.task import PipelineParams, PipelineTask, PipelineTaskSink, PipelineTaskSource
+
 # Check if we're in local development mode
 LOCAL_RUN = os.getenv("LOCAL_RUN")
 if LOCAL_RUN:
@@ -29,10 +45,83 @@ if LOCAL_RUN:
     try:
         from local_runner import configure
     except ImportError:
-        logger.error("Could not import local_runner module. Local development mode may not work.")
+        logger.error(
+            "Could not import local_runner module. Local development mode may not work."
+        )
 
 # Load environment variables
 load_dotenv(override=True)
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+class CustomObserver(BaseObserver):
+    async def on_push_frame(self, data: FramePushed):
+        src = data.source
+        dst = data.destination
+        frame = data.frame
+        direction = data.direction
+        timestamp = data.timestamp
+
+        # Create direction arrow
+        arrow = "→" if direction == FrameDirection.DOWNSTREAM else "←"
+
+        color_start = (
+            bcolors.OKGREEN
+            if direction == FrameDirection.DOWNSTREAM
+            else bcolors.OKBLUE
+        )
+
+        if isinstance(dst, PipelineTaskSink) or isinstance(dst, PipelineTaskSource):
+            if isinstance(frame, BotStartedSpeakingFrame):
+                print(
+                    f"🤖 {color_start} start {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+            elif isinstance(frame, BotStoppedSpeakingFrame):
+                print(
+                    f"🤖 {color_start} STOP {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+            elif isinstance(frame, UserStartedSpeakingFrame):
+                print(
+                    f"😳 {color_start} start {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+            elif isinstance(frame, UserStoppedSpeakingFrame):
+                print(
+                    f"😳 {color_start} STOP {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+
+            elif isinstance(frame, EndFrame):
+                print(
+                    f"📌 {color_start} {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+            elif isinstance(frame, EndTaskFrame):
+                print(
+                    f"🔥 {color_start} {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+
+            elif isinstance(frame, ErrorFrame):
+                print(
+                    f"💔😭 {color_start} {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
+            elif isinstance(frame, LLMFullResponseStartFrame):
+                print(
+                    f"🧠 {color_start} {frame}: {src} {arrow} {dst} {bcolors.ENDC}",
+                    flush=True,
+                )
 
 
 async def main(room_url: str, token: str):
@@ -51,13 +140,14 @@ async def main(room_url: str, token: str):
         DailyParams(
             audio_out_enabled=True,
             transcription_enabled=True,
-            vad_enabled=True,
+            audio_in_enabled=True,
             vad_analyzer=SileroVADAnalyzer(),
         ),
     )
 
     tts = CartesiaTTSService(
-        api_key=os.getenv("CARTESIA_API_KEY"), voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22"
+        api_key=os.getenv("CARTESIA_API_KEY"),
+        voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22",
     )
 
     llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
@@ -65,7 +155,7 @@ async def main(room_url: str, token: str):
     messages = [
         {
             "role": "system",
-            "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way. You know a lot about laserdiscs.",
+            "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way. You know a lot about laserdiscs. Always start with 'Aloha'",
         },
     ]
 
@@ -91,6 +181,9 @@ async def main(room_url: str, token: str):
             enable_usage_metrics=True,
             report_only_initial_ttfb=True,
         ),
+        observers=[
+            CustomObserver(),
+        ],
     )
 
     @transport.event_handler("on_first_participant_joined")
